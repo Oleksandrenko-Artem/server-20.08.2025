dfb40fc6d1c450a10d1796269eea43e639827d9e
Artem Oleksandrenko <artem2008.zp@gmail.com>
2025-08-26 18:37:38 +0300

    Request

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.end('hi from express!');
+app.get('/products/:id', (req, res) => {
+    const requestStr = `method: ${req.method},
+    <br> url: ${req.url},
+    <br> path: ${req.path}, 
+    <br> query: ${JSON.stringify(req.query)},
+    <br> params: ${JSON.stringify(req.params)},
+    <br> key: ${req.params.id},
+    <br> hostname: ${req.hostname},
+    <br> ip: ${req.ip}`
+    console.log(req);
+    res.end(`<h1>${requestStr}</h1>`);
 });
 
-app.get('/about', (req, res) => {
-    res.end('about page');
-});
-
-app.get('/contacts', (req, res) => {
-    res.end('contacts page');
-});
-
-/*-----------------*/
-
-app.use((req, res) => {
-    res.end('error 404');
+app.get('/users/:id', (req, res) => {
+    const requestStr = `${req.path}
+    <br>${req.params.id}`
+    res.end(`<h1>${requestStr}</h1>`);
 });
 
 module.exports = app;
