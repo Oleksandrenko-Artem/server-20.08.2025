cd2c5dfebcc93e4db0187f6480febfe4483227d3
Artem Oleksandrenko <artem2008.zp@gmail.com>
2025-08-26 19:42:13 +0300

    Middleware

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.get('/products/:id', (req, res) => {
-    const requestStr = `method: ${req.method},
-    <br> url: ${req.url},
-    <br> path: ${req.path}, 
-    <br> query: ${JSON.stringify(req.query)},
-    <br> params: ${JSON.stringify(req.params)},
-    <br> key: ${req.params.id},
-    <br> hostname: ${req.hostname},
-    <br> ip: ${req.ip}`
-    console.log(req);
-    res.end(`<h1>${requestStr}</h1>`);
-});
-
-app.get('/users/:id', (req, res) => {
-    const requestStr = `${req.path}
-    <br>${req.params.id}`
-    res.end(`<h1>${requestStr}</h1>`);
+app.get('/', (req, res, next) => {
+    // res.status(200).send('responce');
+    console.log('first');
+    req.test = 5;
+    next();
+}, (req, res, next) => {
+    console.log('second');
+    req.test *= 4;
+    if (true) {
+        next();
+    } else {
+        res.send('stop');
+    }
+}, (req, res, next) => {
+    console.log('third');
+    res.status(200).send('finish, ' + req.test);
 });
 
 module.exports = app;
