157dd433250ff2f36ffa97f7ecf6551f7941dbfa
Artem Oleksandrenko <artem2008.zp@gmail.com>
2025-08-29 23:46:15 +0300

    CRUD

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,6 +5,51 @@ module.exports.createUser = async (req, res, next) => {
         const newUser = await User.create(req.body);
         res.status(201).send({ data: newUser });
     } catch (error) {
-        console.log(error);
+        next(error);
     }
-}
+};
+
+module.exports.findAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        res.status(200).send({ data: users });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports.findUserById = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.idUser);
+        if (!user) {
+            return res.status(404).send({ data: 'User not found' });
+        }
+        res.status(200).send({ data: user });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports.updateUserById = async (req, res, next) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.idUser, req.body, { new: true });
+        if (!updatedUser) {
+            return res.status(404).send({ data: 'User not found' });
+        }
+        res.status(200).send({ data: updatedUser });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports.deleteUserById = async (req, res, next) => {
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.params.idUser);
+        if (!deletedUser) {
+            return res.status(404).send({ data: 'User not found' });
+        }
+        res.status(200).send({ data: deletedUser });
+    } catch (error) {
+        next(error);
+    }
+};
